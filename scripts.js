6970f84231b8971993a486b352f1735d3b2d963c
zemownkee <zemownkee@gmail.com>
2022-11-18 15:49:50 -0500

    Build number entry function

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 //global variables
 let selectedOperation;
-let storedValue = [];
-let currentValue = [];
+let storedValue = '';
+let currentValue = '';
 let displayValue;
 
-//set document references and listeners
+//set document references
+const buttons = document.querySelectorAll('button');
+const buttonArray = [...buttons];
+const numbers = document.querySelectorAll('.number');
+const numberArray = [...numbers];
+
+//set dynamic 'click' listeners that push element.textContent to string
+//get entry as numbers are entered
+numbers.forEach((number) => number.addEventListener('click', () => addEntry(number.textContent)));
+
+function addEntry(entry) {
+    currentValue = currentValue + entry;
+    console.log(currentValue);
+}
 
 //set listeners for keyboard entry
+    //no earthly clue
 
-//get entry as numbers are entered
+
 
 //clean entry
 function clean(array) {return parseFloat(array.join(''));}
