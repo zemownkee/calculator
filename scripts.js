2e9dcc5ae214d8f72d91ffbea8fce24a0c692249
zemownkee <zemownkee@gmail.com>
2022-11-17 19:58:02 -0500

    Initialize files and start pseudocoding

---
+++ scripts.js
@@ -0,0 +1,22 @@
+//global variables
+let selectedOperation;
+let storedValue = [];
+let currentValue = [];
+let displayValue;
+
+//set document references and listeners
+
+//set listeners for keyboard entry
+
+//get entry as numbers are entered
+
+//clean entry
+function clean(array) {return parseFloat(array.join(''));}
+
+//get selection operation and store
+
+//switch from entering first number to entering second number
+
+//perform operation on cleaned inputs
+
+//reset values/display
